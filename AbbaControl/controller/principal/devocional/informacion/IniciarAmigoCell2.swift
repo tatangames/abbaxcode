ead8985b191bac6e9c56fbf59c19322bcb5ae628
Jonathan Moran <tatangamess@gmail.com>
2024-03-20 14:20:41 -0600

    iniciar pantalla mis planes

---
+++ AbbaControl/controller/principal/devocional/informacion/IniciarAmigoCell2.swift
@@ -0,0 +1,8 @@
+//
+//  IniciarAmigoCell2.swift
+//  AbbaControl
+//
+//  Created by Jonathan  Moran on 20/3/24.
+//
+
+import Foundation
---
+++ AbbaControl/modelos/devocionales/ModeloComuAmigosIniciar.swift
@@ -0,0 +1,8 @@
+//
+//  ModeloComuAmigosIniciar.swift
+//  AbbaControl
+//
+//  Created by Jonathan  Moran on 20/3/24.
+//
+
+import Foundation
